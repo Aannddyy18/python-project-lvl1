10148e5efae44d0d6ac100fa1f44ccfa7e2f9201
Андрей Трушин <trand1@mail.ru>
2021-12-30 21:56:58 +0700

    final homework lvl1

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,23 +7,24 @@ MIN_START = 1
 MAX_START = 3
 MIN_STEP = 1
 MAX_STEP = 3
-QUANTITY_ELEM = 15
+QUANTITY_ELEM = 10
 
 
 def form_task_and_answer():
     """Let's calculate."""
-    progression, hidden_index, hidden = form_progression()
+    initial_term = randint(MIN_START, MAX_START)
+    common_difference = randint(MIN_STEP, MAX_STEP)
+    end_term = (initial_term + common_difference * (QUANTITY_ELEM - 1))
+    progression = form_progression(initial_term, end_term, common_difference)
+    hidden_index = randint(0, QUANTITY_ELEM - 1)
+    hidden = progression[hidden_index]
     task = stringify_progression(progression, hidden_index)
     return task, hidden
 
 
-def form_progression():
-    start_progression = randint(MIN_START, MAX_START)
-    step_progression = randint(MIN_STEP, MAX_STEP)
-    progression = range(start_progression, QUANTITY_ELEM + 1, step_progression)
-    hidden_index = randint(0, len(progression) - 1)
-    hidden = progression[hidden_index]
-    return progression, hidden_index, hidden
+def form_progression(initial_term, end_term, common_difference):
+    progression = range(initial_term, end_term + 1, common_difference)
+    return progression
 
 
 def stringify_progression(progression, hidden_index):
